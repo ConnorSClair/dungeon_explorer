92353e9e2f7d0743ed795e099fe1fe6de1fc671b
Connor Macleod <connormusique@gmail.com>
2019-05-10 08:15:59 +1000

    MVP map with explorer and goal and user input to move around map

--- structures.py
+++ structures.py
@@ -3,22 +3,18 @@ from enum import *
 import logging
 
 class Move(Enum):
-    LEFT = (-1,0)
-    RIGHT = (1,0)
-    DOWN = (0,1)
-    UP = (0,-1)
+    UP = (-1,0)
+    DOWN = (1,0)
+    RIGHT = (0,1)
+    LEFT = (0,-1)
 
 class Dungeon_Master:
     def __init__(self, n: int, m: int):
         self.dungeon = Dungeon(n,m,0,(0,0),(n-1,m-1)) 
-        self.move_explorer(Move.RIGHT)
         
     def move_explorer(self,move: Move):
-        self.dungeon.move_entity(self.dungeon.explorer_loc,move)
+        self.dungeon.move_explorer(move)
         print(self.dungeon)
-    
-
-
 
 class Dungeon:
     def __init__(self, n: int, m: int,obsticles: int, start_loc:Tuple[int,int], goal_loc:Tuple[int,int]):
@@ -32,6 +28,7 @@ class Dungeon:
         self.build_board(obsticles)
         self.set_entity(start_loc,'o')
         self.set_entity(goal_loc,'*')
+        print(self)
 
     def __repr__(self):
         result: List[List[str]] = []
@@ -62,22 +59,24 @@ class Dungeon:
             self.entities.append(row)
         logging.info(self.entities)
     
-    def move_entity(self, start_loc, move: Move):
-        sym: str = self.get_cell(start_loc,self.entities)
-        self.set_cell(start_loc,None,self.entities)
+    def move_explorer(self, move: Move):
+        sym: str = self.get_cell(self.explorer_loc,self.entities)
         # check if valid
-        end_loc = (start_loc[0]+move[0],start_loc[1]+move[1])
-        self.set_cell(end_loc,sym,self.entities)
+        end_loc = (self.explorer_loc[0]+move.value[0],self.explorer_loc[1]+move.value[1])
+        if self.set_cell(end_loc,sym,self.entities):
+            self.set_cell(self.explorer_loc,None,self.entities)
+            self.explorer_loc = end_loc
+        logging.info("{} moved from {} to {}".format(sym,self.explorer_loc,end_loc))
 
     def set_entity(self, loc:Tuple[int,int],sym: str): 
         if self.valid_loc(loc):
             self.entities[loc[0]][loc[1]] = sym
             logging.info(self.entities)
         else:
-            logging.warn("invalid location {}".format(loc))
+            logging.warning("invalid location {}".format(loc))
     
     def valid_move(self, loc: Tuple[int,int], move: Move):
-        potential_move = (loc[0] + move[0],loc[1] + move[1])
+        potential_move = (loc[0] + move.value[0],loc[1] + move.value[1])
         return self.valid_loc(potential_move)
     
     def valid_loc(self,loc: Tuple[int,int]):
@@ -92,18 +91,17 @@ class Dungeon:
             result = map[loc[0]][loc[1]]
             logging.info("got {} with value {}".format(loc,result))
         else:
-            logging.warn("invalid location {}".format(loc))
+            logging.warning("invalid location {}".format(loc))
         return result
-
-    def move_cell(self):
-        return False
 
     def set_cell(self, loc: Tuple[int,int], value: str, map: List[List[str]]):
         if self.valid_loc(loc):
             map[loc[0]][loc[1]] = value
             logging.info("set {} with value {}".format(loc,value))
+            return True
         else:
-            logging.warn("invalid location {}".format(loc))
+            logging.warning("invalid location {}".format(loc))
+            return False
 
 class Explorer:
     @staticmethod
@@ -113,10 +111,25 @@ class Explorer:
 
 if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO,
+    logging.basicConfig(format=format, level=logging.WARNING,
                         datefmt="%H:%M:%S")
+    
     dungeon_master = Dungeon_Master(14,8)
-    print(dungeon_master.dungeon)
+    
+    while True:
+        choice = input("move - {left,right,up,down} or quit - {quit}")
+        if choice == "quit":
+            break
+        else:
+            if choice == "left":
+                dungeon_master.move_explorer(Move.LEFT)
+            if choice == "right":
+                dungeon_master.move_explorer(Move.RIGHT)
+            if choice == "up":
+                dungeon_master.move_explorer(Move.UP)
+            if choice == "down":
+                dungeon_master.move_explorer(Move.DOWN)
+            
 
 
 
